72d92369ec704cfb17925d73af2811b562cd602e
frilachraf <frilachraf.dev@gmail.com>
2025-03-18 09:06:33 +0000

    added successfully

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,14 @@
+import {heroui} from '@heroui/theme';
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-      './pages/**/*.{js,ts,jsx,tsx,mdx}',
-      './components/**/*.{js,ts,jsx,tsx,mdx}',
-      './app/**/*.{js,ts,jsx,tsx,mdx}',
-    ],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/components/(button|ripple|spinner).js"
+  ],
     theme: {
       extend: {},
     },
-    plugins: [],
+  plugins: [heroui()],
   }
